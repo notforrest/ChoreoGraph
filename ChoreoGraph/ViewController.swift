aa39a9b46f7a79c1fd71eca75e3c4b0b48b81265
Forrest Sun <Ddeosun@gmail.com>
2024-03-08 21:48:25 -0600

    Initial Commit

---
+++ ChoreoGraph/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChoreoGraph
+//
+//  Created by Forrest Sun on 3/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
