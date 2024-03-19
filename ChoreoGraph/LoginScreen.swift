f8c568e2f02c12f7749c671e8ee301f8acefe574
kathyxcui <83180722+kathyxcui@users.noreply.github.com>
2024-03-18 21:20:19 -0500

    login screen functionality

--- ChoreoGraph/LoginScreen.swift
+++ ChoreoGraph/LoginScreen.swift
@@ -9,6 +9,35 @@ import UIKit
 
 class LoginScreen: UIViewController {
     
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func LoginButtonPressed(_ sender: Any) {
+        guard let username = usernameTextField.text, !username.isEmpty,
+                  let password = passwordTextField.text, !password.isEmpty else {
+                // alert - add functionality later
+                return
+            }
+        
+        performSegue(withIdentifier: "LoginSegue", sender: self)
+    }
+    
+    @IBAction func SignUpButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "SignUpSegue", sender: self)
+    }
+    
+    // 'return' key
+    func textFieldShouldReturn(_ textField:UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // dismiss keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
