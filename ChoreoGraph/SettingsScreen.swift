8add43bb5829b46c1b3d51b7f0794330ce46c752
lizagna <lizagna@Elizabeths-MacBook-Pro.local>
2024-03-18 21:29:03 -0500

    added setting/user profile screen

---
+++ ChoreoGraph/SettingsScreen.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsScreen.swift
+//  ChoreoGraph
+//
+//  Created by Elizabeth Luu on 3/18/24.
+//
+
+import UIKit
+
+class SettingsScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
